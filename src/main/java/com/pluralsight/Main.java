99ef5b2519e5756a1d1a9b20ee5e889eba0bfba0
Ahsan <sbaseer25@gmail.com>
2025-09-25 13:16:23 -0400

    Completed Q2

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,8 +2,21 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        //Question 1:
+        int bobSalary = 50000;
+        int garySalary = 90000;
+        int highestSalary;
 
+        highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is: " + highestSalary);
 
+        //Question 2:
+        int carPrice = 94092;
+        int truckPrice = 5456;
+
+        System.out.println("The smallest price is: " + Math.min(carPrice, truckPrice));
+
+        //Question 3:
 
     }
 }
